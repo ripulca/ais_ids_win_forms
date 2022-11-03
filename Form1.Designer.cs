720cdbeb7d1b0cfd3bc12972765b621c0d38720d
ripulca <68591785+ripulca@users.noreply.github.com>
2022-11-04 01:28:57 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ais_ids
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.domain = new System.Windows.Forms.TextBox();
+            this.ipaddr = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(59, 154);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(686, 284);
+            this.output.TabIndex = 0;
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(96, 38);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 1;
+            // 
+            // domain
+            // 
+            this.domain.Location = new System.Drawing.Point(323, 101);
+            this.domain.Name = "domain";
+            this.domain.Size = new System.Drawing.Size(100, 20);
+            this.domain.TabIndex = 2;
+            // 
+            // ipaddr
+            // 
+            this.ipaddr.Location = new System.Drawing.Point(96, 101);
+            this.ipaddr.Name = "ipaddr";
+            this.ipaddr.Size = new System.Drawing.Size(100, 20);
+            this.ipaddr.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(323, 38);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 4;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(572, 40);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(115, 17);
+            this.checkBox.TabIndex = 5;
+            this.checkBox.Text = "Удаленный хост?";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(572, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Получить ПО";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Имя пользователя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Пароль:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "IP адрес:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(320, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Домен:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.ipaddr);
+            this.Controls.Add(this.domain);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox domain;
+        private System.Windows.Forms.TextBox ipaddr;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ais_ids
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> GetHardwareInfo()
+        {
+            ConnectionOptions connection = new ConnectionOptions();
+
+            List<string> result = new List<string>();
+
+            try
+            {
+
+                ManagementObjectSearcher searcher;
+
+                if ((bool)checkBox.Checked)
+                {
+                    connection.Username = username.Text;
+                    connection.Password = password.Text;
+                    connection.Authority = "ntlmdomain:" + domain.Text;
+
+                    ManagementScope scope = new ManagementScope("\\\\" + ipaddr.Text + "\\root\\CIMV2", connection);
+                    scope.Connect();
+
+                    ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_SoftwareElement");
+
+                    searcher = new ManagementObjectSearcher(scope, query);
+                }
+                else
+                {
+                    searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
+                }
+                string text = "";
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    if (obj["InstallSource"] != null)
+                    {
+                        if (obj["Name"] != null)
+                        {
+                            text += "ProductName: " + obj["Name"].ToString().Trim() + "\n";
+                        }
+                        else
+                        {
+                            text += "PackageName: \n";
+                        }
+                        text += "Version: " + obj["Version"].ToString().Trim() + "\n";
+                        if (obj["InstallLocation"] != null)
+                        {
+                            text += "InstallLocation: " + obj["InstallLocation"].ToString().Trim() + "\n";
+                        }
+                        else
+                        {
+                            text += "InstallLocation: \n";
+                        }
+                        text += "InstallSource: " + obj["InstallSource"].ToString().Trim() + "\n";
+                        text += "Vendor: " + obj["Vendor"].ToString().Trim() + "\n";
+                        text += "\n";
+                    }
+                }
+
+                output.Text = text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetHardwareInfo();
+        }
+    }
+}
